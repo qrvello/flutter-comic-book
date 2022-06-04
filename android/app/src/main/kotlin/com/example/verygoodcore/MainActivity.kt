57f6838c199ff591ed8e9e3ca7c2870ff61a65ea
Santiago Curvello <santicurvello@gmail.com>
2022-06-03 22:07:17 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.comic_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:comic_book/app/app.dart';
+import 'package:comic_book/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
